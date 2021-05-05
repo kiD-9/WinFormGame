59e65a3fb98624fc66f9fe7e71fb2a3e4befc0ff
kiD-9 <d.kirill.ekb@gmail.com>
2021-05-05 16:05:38 +0500

    Added context menu to interact with field

--- GoldenCity/GoldenCity.Forms/Form1.cs
+++ GoldenCity/GoldenCity.Forms/Form1.cs
@@ -14,64 +14,150 @@ namespace GoldenCity.Forms
 {
     public partial class Form1 : Form
     {
-        private Dictionary<string, Bitmap> bitmaps = new Dictionary<string, Bitmap>();
-        private int bitmapSize = 175;
-        private int mapSize = 5;
-        private GameSetting gameSetting;
+        private readonly Dictionary<string, Bitmap> bitmaps = new Dictionary<string, Bitmap>();
+        private const int GamePropertiesBarHeight = 40;
+        private const int BorderLineWidth = 8;
+        private const int BitmapSize = 120;
+        private const int MapSize = 5;
+        private readonly GameSetting gameSetting;
         
-        public Form1() //TODO я только попробовал поотрисовывать элементы карты
+        public Form1()
         {
             DoubleBuffered = true;
             InitializeComponent();
             
-            
-            gameSetting = new GameSetting(mapSize, 10000, true);
-            gameSetting.AddBuilding(new SheriffsHouse(3, 2)); //почему он не добавляется
-            gameSetting.AddBuilding(new Store(1, 0));
-            gameSetting.AddBuilding(new LivingHouse(2,0));
-            ClientSize = new Size(mapSize * bitmapSize, mapSize * bitmapSize + 30);
-            
-            var imagesDirectory = new DirectoryInfo("Resources");
-            foreach (var fileInfo in imagesDirectory.GetFiles("*.png"))
+            gameSetting = new GameSetting(MapSize, 10000, true);
+            ClientSize = new Size(MapSize * BitmapSize, MapSize * BitmapSize + GamePropertiesBarHeight);
+            for (var i = 0; i < 4; i++)
             {
-                bitmaps[fileInfo.Name] = (Bitmap) Image.FromFile(fileInfo.FullName);
+                gameSetting.AddCitizien(null);
             }
 
-            AddControls();
+            TakeBitmapsFromDirectory(new DirectoryInfo("Resources"));
+            
+            AddPropertiesControls();
             
             var repaintTimer = new Timer {Interval = 15};
             repaintTimer.Tick += TimerTick;
             repaintTimer.Start();
-            //
-            // Click += HandleClick;
-            // DoubleClick += HandleClick;
+            
+            //TODO игровые таймеры
+            
+            Click += HandleClick;
         }
 
         protected  override void OnPaint(PaintEventArgs e)
         {
-            UpdateGamePropertiesPaint(); //TODO переделать костыль
             DrawBackground(e.Graphics);
             DrawBuildings(e.Graphics);
+            UpdateGamePropertiesValues();
         }
 
         private void HandleClick(object sender, EventArgs e) //TODO
         {
             var clickArgs = e as MouseEventArgs;
+            if (clickArgs.Y <= GamePropertiesBarHeight)
+                return;
+
+            var buildingLocation = new Point(clickArgs.X / BitmapSize,
+                (clickArgs.Y - GamePropertiesBarHeight) / BitmapSize);
+            
+            var toolStripMenuItemJail = new ToolStripMenuItem("Jail", null,
+                (o, args) =>
+                    gameSetting.AddBuilding(new Jail(buildingLocation.X, buildingLocation.Y)))
+            {
+                BackColor = Color.Chocolate
+            };
+            var toolStripMenuItemLivingHouse = new ToolStripMenuItem("Living house", null,
+                (o, args) =>
+                    gameSetting.AddBuilding(new LivingHouse(buildingLocation.X, buildingLocation.Y)))
+            {
+                BackColor = Color.Chocolate
+            };
+            var toolStripMenuItemRailroadStation = new ToolStripMenuItem("Railroad station", null,
+                (o, args) =>
+                    gameSetting.AddBuilding(new RailroadStation(buildingLocation.X, buildingLocation.Y)))
+            {
+                BackColor = Color.Chocolate
+            };
+            var toolStripMenuItemSaloon = new ToolStripMenuItem("Saloon", null,
+                (o, args) =>
+                    gameSetting.AddBuilding(new Saloon(buildingLocation.X, buildingLocation.Y)))
+            {
+                BackColor = Color.Chocolate
+            };
+            var toolStripMenuItemSheriffsHouse = new ToolStripMenuItem("Sheriffs house", null,
+                (o, args) =>
+                    gameSetting.AddBuilding(new SheriffsHouse(buildingLocation.X, buildingLocation.Y)))
+            {
+                BackColor = Color.Chocolate
+            };
+            var toolStripMenuItemStore = new ToolStripMenuItem("Store", null,
+                (o, args) =>
+                    gameSetting.AddBuilding(new Store(buildingLocation.X, buildingLocation.Y)))
+            {
+                BackColor = Color.Chocolate
+            };
+
+            var toolStripMenuItemAddBuilding = new ToolStripMenuItem("Add building")
+            {
+                DropDownItems =
+                {
+                    toolStripMenuItemJail, toolStripMenuItemLivingHouse,
+                    toolStripMenuItemRailroadStation, toolStripMenuItemSaloon,
+                    toolStripMenuItemSheriffsHouse, toolStripMenuItemStore
+                },
+                BackColor = Color.Chocolate
+            };
+            
+            
+            var toolStripMenuItemDeleteBuilding = new ToolStripMenuItem("Delete building", null,
+                (s, args) => 
+                    gameSetting.DeleteBuilding(buildingLocation.X, buildingLocation.Y))
+            {
+                BackColor = Color.Chocolate
+            };
+
+            var toolStripMenuItemAddWorker = new ToolStripMenuItem("Add worker", null,
+                (s, args) => 
+                    gameSetting.AddWorker(gameSetting.Map[buildingLocation.Y, buildingLocation.X]))
+            {
+                BackColor = Color.Chocolate
+            };
+
+            var toolStripMenuItemRetireWorker = new ToolStripMenuItem("Retire worker", null,
+                (s, args) =>
+                    gameSetting.RetireWorker(gameSetting.Map[buildingLocation.Y, buildingLocation.X].WorkerId))
+            {
+                BackColor = Color.Chocolate
+            };
+
+
+            var contextMenuStrip = new ContextMenuStrip
+            {
+                Items =
+                {
+                    toolStripMenuItemAddBuilding, toolStripMenuItemDeleteBuilding,
+                    toolStripMenuItemAddWorker, toolStripMenuItemRetireWorker
+                },
+                BackColor = Color.Chocolate
+            };
+            contextMenuStrip.Show(clickArgs.Location);
         }
 
         private void TimerTick(object sender, EventArgs e)
         {
             gameSetting.ChangeMoney(100);// для проверки отрисовки добавил доход
-            Invalidate(); //но значение почему-то не меняется и не рисуется
+            Invalidate();
         }
 
         private void DrawBackground(Graphics graphics)
         {
-            for (var i = 0; i < mapSize * mapSize; i++)
+            for (var i = 0; i < MapSize * MapSize; i++)
             {
-                var point = new Point(bitmapSize * (i % mapSize), bitmapSize * (i / mapSize) + 30);
-                graphics.DrawImage(bitmaps["ground.png"], point);
-                graphics.DrawRectangle(new Pen(Color.Sienna, 8), new Rectangle(point, new Size(bitmapSize, bitmapSize)));
+                var point = new Point(BitmapSize * (i % MapSize), BitmapSize * (i / MapSize) + GamePropertiesBarHeight);
+                graphics.DrawImage(bitmaps["Background.png"], point);
+                graphics.DrawRectangle(new Pen(Color.Sienna, BorderLineWidth), new Rectangle(point, new Size(BitmapSize, BitmapSize)));
             }
         }
 
@@ -80,7 +166,7 @@ namespace GoldenCity.Forms
             var counter = 0;
             foreach (var building in gameSetting.Map)
             {
-                var point = new Point(bitmapSize * (counter % mapSize), bitmapSize * (counter / mapSize) + 30);
+                var point = new Point(BitmapSize * (counter % MapSize), BitmapSize * (counter / MapSize) + GamePropertiesBarHeight);
                 switch (building)
                 {
                     case Jail:
@@ -106,71 +192,70 @@ namespace GoldenCity.Forms
             }
         }
 
-        private void AddControls()
+        private void AddPropertiesControls()
         {
             var money = new Label
             {
                 Location = new Point(0, 0),
-                Size = new Size(ClientSize.Width / 5, 26),
+                Size = new Size(ClientSize.Width / MapSize, GamePropertiesBarHeight),
                 Text = $"Money: {gameSetting.Money}",
+                BackColor = Color.Chocolate
             };
-            //money.TextChanged += MoneyTextChanged; //не совсем понимаю как текст обновлять
 
             var citiziensCount = new Label
             {
-                Location = new Point(ClientSize.Width / 5, 0),
-                Size = new Size(ClientSize.Width / 5, 26),
-                Text = $"Citiziens: {gameSetting.citiziens.Count}"
+                Location = new Point(ClientSize.Width / MapSize, 0),
+                Size = new Size(ClientSize.Width / MapSize, GamePropertiesBarHeight),
+                Text = $"Citiziens: {gameSetting.citiziens.Count}",
+                BackColor = Color.Chocolate
             };
-            //citiziensCount.TextChanged += CitiziensCountTextChanged;
 
             var citiziensLimit = new Label
             {
-                Location = new Point(2 * ClientSize.Width / 5, 0),
-                Size = new Size(ClientSize.Width / 5, 26),
-                Text = $"Citiziens Limit: {gameSetting.CitiziensLimit}"
+                Location = new Point(2 * ClientSize.Width / MapSize, 0),
+                Size = new Size(ClientSize.Width / MapSize, GamePropertiesBarHeight),
+                Text = $"Citiziens Limit: {gameSetting.CitiziensLimit}",
+                BackColor = Color.Chocolate
             };
-            //citiziensLimit.TextChanged += CitiziensLimitTextChanged;
 
             var sheriffsCount = new Label
             {
-                Location = new Point(3 * ClientSize.Width / 5, 0),
-                Size = new Size(ClientSize.Width / 5, 26),
+                Location = new Point(3 * ClientSize.Width / MapSize, 0),
+                Size = new Size(ClientSize.Width / MapSize, GamePropertiesBarHeight),
                 Text = $"Sheriffs: {gameSetting.SheriffsCount}",
+                BackColor = Color.Chocolate
             };
-            //sheriffsCount.TextChanged += SheriffsCountTextChanged;
+
+            var attackTimer = new Label
+            {
+                Location = new Point(4 * ClientSize.Width / MapSize, 0),
+                Size = new Size(ClientSize.Width / MapSize, GamePropertiesBarHeight),
+                Text = $"Attack timer: {gameSetting.AttackTimerInterval / 1000} sec", // можно попробовать сделать динамически меняющимся
+                BackColor = Color.Chocolate
+            };
             
             Controls.Add(money);
             Controls.Add(citiziensCount);
             Controls.Add(citiziensLimit);
             Controls.Add(sheriffsCount);
+            Controls.Add(attackTimer);
         }
 
-        private void UpdateGamePropertiesPaint()
+        private void UpdateGamePropertiesValues()
         {
             Controls[0].Text = $"Money: {gameSetting.Money}";
             Controls[1].Text = $"Citiziens: {gameSetting.citiziens.Count}";
             Controls[2].Text = $"Citiziens Limit: {gameSetting.CitiziensLimit}";
             Controls[3].Text = $"Sheriffs: {gameSetting.SheriffsCount}";
+            Controls[4].Text = $"Attack timer: {gameSetting.AttackTimerInterval / 1000} sec";
         }
-        // private void MoneyTextChanged(object sender, EventArgs e)
-        // {
-        //     Controls[0].Text = $"Money: {gameSetting.Money}";
-        // }
-        //
-        // private void CitiziensCountTextChanged(object sender, EventArgs e)
-        // {
-        //     Controls[1].Text = $"Citiziens: {gameSetting.citiziens.Count}";
-        // }
-        //
-        // private void CitiziensLimitTextChanged(object sender, EventArgs e)
-        // {
-        //     Controls[2].Text = $"Citiziens Limit: {gameSetting.CitiziensLimit}";
-        // }
-        //
-        // private void SheriffsCountTextChanged(object sender, EventArgs e)
-        // {
-        //     Controls[3].Text = $"Sheriffs: {gameSetting.SheriffsCount}";
-        // }
+
+        private void TakeBitmapsFromDirectory(DirectoryInfo imagesDirectoryInfo)
+        {
+            foreach (var fileInfo in imagesDirectoryInfo.GetFiles("*.png"))
+            {
+                bitmaps[fileInfo.Name] = (Bitmap) Image.FromFile(fileInfo.FullName);
+            }
+        }
     }
 }
